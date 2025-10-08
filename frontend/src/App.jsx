27b54bed717483ae40905625582452153a40edfb
johnmartinroque <johnmartinroque@gmail.com>
2025-10-08 14:44:07 +0800

    tailwind setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import BackendStatus from "./components/BackendStatus";
 function App() {
   return (
     <div>
+      <h1 className="text-5xl flex flex-col items-center">something</h1>
       <BackendStatus />
     </div>
   );
